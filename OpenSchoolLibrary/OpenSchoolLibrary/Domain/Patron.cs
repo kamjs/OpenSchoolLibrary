9ae9ec78112a5fa71d8d40df9f7f3f19cf3178c4
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-04-11 17:18:22 -0400

    Add Domain folder and create Patron Entity inside.

---
+++ OpenSchoolLibrary/OpenSchoolLibrary/Domain/Patron.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenSchoolLibrary.Domain
+{
+    public class Patron
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+        public string HomeRoomTeacher { get; set; }
+        public string LibraryCard { get; set; }
+        public string OptionalSecret { get; set; }
+
+    }
+}
