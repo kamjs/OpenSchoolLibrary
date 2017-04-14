b28a1af584b58669a73fcc321b324462eae2325b
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-04-14 17:02:46 -0400

    Add properties to Create Book Model and set Create Book View to use it.

--- OpenSchoolLibrary/OpenSchoolLibrary/Models/BooksViewModels/CreateBookViewModel.cs
+++ OpenSchoolLibrary/OpenSchoolLibrary/Models/BooksViewModels/CreateBookViewModel.cs
@@ -7,5 +7,14 @@ namespace OpenSchoolLibrary.Models.BooksViewModels
 {
     public class CreateBookViewModel
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Author { get; set; }
+        public int ISBN { get; set; }
+        public int ISBN13 { get; set; }
+        public int Condition { get; set; }
+        public string CatalogID { get; set; }
+        public string Genre { get; set; }
     }
 }
