8fd4cd86f4383ff03bc92a0ab3ec23a2f89dd4b4
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-04-14 08:46:58 -0400

    Update LibraryContext to use new Connection String.

--- OpenSchoolLibrary/OpenSchoolLibrary/Domain/LibraryContext.cs
+++ OpenSchoolLibrary/OpenSchoolLibrary/Domain/LibraryContext.cs
@@ -4,6 +4,7 @@ using System.Data.Entity.Infrastructure;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace OpenSchoolLibrary.Domain
 {
