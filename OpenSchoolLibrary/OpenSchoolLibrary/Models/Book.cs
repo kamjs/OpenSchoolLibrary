14b8a62eb57a9173a0abd6978a8db5c7ca135340
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-04-14 15:04:53 -0400

    Add Book Model.

---
+++ OpenSchoolLibrary/OpenSchoolLibrary/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenSchoolLibrary.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Author { get; set; }
+        public int ISBN { get; set; }
+        public int ISBN13 { get; set; }
+        public int Condition { get; set; }
+        public string CatalogID { get; set; }
+        public int Genre { get; set; }
+    }
+}
