377c8753be4d4da4aa09cc4b61f01092faa9b1a3
Christopher C. Johnson <chris@thatamazingprogrammer.com>
2017-04-15 12:42:02 -0400

    Add DataAnnotations.

--- OpenSchoolLibrary/OpenSchoolLibrary/Models/BooksViewModels/AddBookViewModel.cs
+++ OpenSchoolLibrary/OpenSchoolLibrary/Models/BooksViewModels/AddBookViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,13 +10,29 @@ namespace OpenSchoolLibrary.Models.BooksViewModels
     public class AddBookViewModel
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is Required")]
         public string Title { get; set; }
+
+        [Display(Name = "Subtitle")]
         public string SubTitle { get; set; }
+
+        [Required(ErrorMessage = "Author is Required")]
         public string Author { get; set; }
+
         public int ISBN { get; set; }
+
+        [Display(Name = "ISBN-13")]
         public int ISBN13 { get; set; }
+
+        [Required(ErrorMessage = "Condition is Required")]
         public int Condition { get; set; }
+
+        [Required(ErrorMessage = "Catalog ID is Required")]
+        [Display(Name = "Catalog ID")]
         public string CatalogID { get; set; }
+
+        [Required(ErrorMessage = "Genre is Required")]
         public int Genre { get; set; }
 
         public SelectList GenreList { get; set; }
